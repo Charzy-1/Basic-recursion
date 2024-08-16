0385dd2f4814adbfb33e2684af2e374c0043561a
Charles <chazylites@gmail.com>
2024-08-16 16:29:18 +0100

    modified my code to handle a case of inputs below or equals 0

--- challenge.js
+++ challenge.js
@@ -1,6 +1,11 @@
 //Fill in the method sum so that it adds up all the positive integers up to number and returns the sum. For example, when given 4 it should return 10 (4+3+2+1).
 
 module.exports = function sum(number) {
+    // Handle the case when the number is zero or negative
+    if (number <= 0) {
+        return 'Invalid input please enter a number greater than 0'; 
+    }
+
     //Base case: if the number is 1 return 1
     if (number === 1) {
         return 1;
@@ -11,4 +16,4 @@ module.exports = function sum(number) {
 
 //Test case
 console.log(sum(4));
-console.log(sum(4));
+console.log(sum(0));
